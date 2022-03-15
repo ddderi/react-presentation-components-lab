5b3fc4b370407ae565dd9b1207f7dae2c2a4db4b
ddderi <maxime830@live.fr>
2022-03-15 04:02:32 +0100

    commit tree aac483705e2023019f8f340a3f2a2c9fd058a8c9

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,14 @@
 // Code SimplerComponent Here
+
+// function SimplerComponent(props) { 
+// return <div onClick={props.handleClick}>I am just happy</div>
+// }
+
+import React from 'react'
+
+const SimplerComponent = props => {
+    return (<div onClick={props.handleClick} >I am just happy</div>)
+}
+
+
+export default SimplerComponent
--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+
+
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={alert('oui')} />
   </div>,
   document.getElementById('root')
 );
